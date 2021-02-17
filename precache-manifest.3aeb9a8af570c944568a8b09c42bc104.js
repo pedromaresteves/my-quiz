0bf3db696e13e829cf7159d42a8e435cc373e0fc
pmarques <pmarques@idealista.com>
2021-02-17 20:16:39 +0100

    Updates

---
+++ precache-manifest.3aeb9a8af570c944568a8b09c42bc104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff60396282ea94f881ac9b499582a3d",
+    "url": "/my-quiz/index.html"
+  },
+  {
+    "revision": "cdb4af3df39763ae19de",
+    "url": "/my-quiz/static/css/main.141150c4.chunk.css"
+  },
+  {
+    "revision": "9e9577d3859045cbe3c9",
+    "url": "/my-quiz/static/js/2.e123d2eb.chunk.js"
+  },
+  {
+    "revision": "f5edd81387c5cbfc485172e86aecddb6",
+    "url": "/my-quiz/static/js/2.e123d2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb4af3df39763ae19de",
+    "url": "/my-quiz/static/js/main.a2851512.chunk.js"
+  },
+  {
+    "revision": "bbe880372bf464c7a5aa",
+    "url": "/my-quiz/static/js/runtime-main.5a8b8c9a.js"
+  }
+]);
